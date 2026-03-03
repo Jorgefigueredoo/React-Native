631c02205997c51ac3303d683bf2df56935245d8
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-03-03 18:03:25 -0300

    ...

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Dale Pirraia</Text>
+      <Text>Olá Mundo</Text>
       <StatusBar style="auto" />
     </View>
   );
